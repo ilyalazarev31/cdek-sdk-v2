b0a0de7582c3b0bf64e2f67724698652b4abd3eb
Lazarev Ilya <35830613+ilyalazarev31@users.noreply.github.com>
2024-06-19 11:16:57 +0400

    Update PackagesResponse.php

--- src/Entity/Responses/PackagesResponse.php
+++ src/Entity/Responses/PackagesResponse.php
@@ -14,12 +14,6 @@ use AntistressStore\CdekSDK2\Traits\PackageTrait;
 class PackagesResponse extends Source
 {
     use PackageTrait;
-    /**
-     *Уникальный номер упаковки в ИС СДЭК.
-     *
-     * @var string
-     */
-    protected $package_id;
 
     /**
      * Объемный вес (в граммах).
@@ -53,15 +47,5 @@ class PackagesResponse extends Source
     public function getWeightCalc()
     {
         return $this->weight_calc;
-    }
-
-    /**
-     * Получить значение - Уникальный номер упаковки в ИС СДЭК.
-     *
-     * @return string
-     */
-    public function getPackageId()
-    {
-        return $this->package_id;
     }
 }
